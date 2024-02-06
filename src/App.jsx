a8773a2341e1944a57a366e9206e394861165805
Brice Cabioch <cabioch.brice@gmail.com>
2024-02-06 11:34:01 +0100

    completed iteration 3

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,44 @@ import { useState } from "react";
 function App() {
   const [people, setPeople] = useState(contacts.slice(0, 5));
 
+  const addRandomContact = () => {
+    // Clone the array (5 contacts)
+    const clonedPeople = [...people];
+    // console.log("clonedPeople", clonedPeople);
+
+    // Filter contacts that are already displayed (52 - 5 = 47 remaining contacts)
+    const remainingContacts = contacts.filter((contact) => {
+      for (let i = 0; i < clonedPeople.length; i++) {
+        if (clonedPeople[i].id === contact.id) {
+          return false;
+        }
+      }
+      return true;
+    });
+    // console.log("allContacts", contacts);
+    // console.log("remainingContacts", remainingContacts);
+
+    // Random selection
+    const randomContact =
+      remainingContacts[Math.floor(Math.random() * remainingContacts.length)];
+    // console.log("randomContact", randomContact);
+
+    // Add random contact to the cloned array
+    clonedPeople.push(randomContact);
+    // console.log("clonedPeople", clonedPeople);
+
+    // Finally, update the people variable through the function
+    setPeople(clonedPeople);
+  };
+
+  console.log("people", people); // See the output of clicking the add random contact button
+
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
+
+      <button onClick={addRandomContact}>Add Random Contact</button>
+
       <table>
         <tbody>
           <tr>
@@ -25,8 +60,8 @@ function App() {
                 </td>
                 <td>{actor.name}</td>
                 <td>{actor.popularity}</td>
-                <td>{actor.wonOscar ? <p>🏆</p> : <p></p>}</td>
-                <td>{actor.wonEmmy ? <p>🌟</p> : <p></p>}</td>
+                <td>{actor.wonOscar && <p>🏆</p>}</td>
+                <td>{actor.wonEmmy && <p>🌟</p>}</td>
               </tr>
             );
           })}
